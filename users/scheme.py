e15e898a9d2765365ceaa195459fdfba661fe8aa
ali sharifi <alisharifyofficial@gmail.com>
2025-04-06 18:50:37 +0330

    :white_check_mark:CRUD over users done

--- users/scheme.py
+++ users/scheme.py
@@ -11,9 +11,27 @@ class CreateUserScheme(BaseModel):
     password: constr(max_length=128)
     email_address: Optional[EmailStr] = None
     phone_number: Optional[constr(max_length=16)] = None
-    last_login: Optional[datetime] = None
-    login_attempts: int = 0
     gender: Optional[Gender] = None
 
     class Config:
         from_attributes = True  # Enables conversion from ORM objects (Pydantic v2)
+
+
+class BaseDumpUserScheme(BaseModel):
+    first_name: str
+    last_name: str
+    username: constr(max_length=256)
+    email_address: EmailStr
+    phone_number: constr(max_length=16)
+    gender: Gender
+
+    class Config:
+        from_attributes = True  # Enables conversion from ORM objects (Pydantic v2)
+
+class DumpUserScheme(BaseDumpUserScheme):
+    public_key: str
+    id: int
+
+class UpdateUserScheme(BaseDumpUserScheme):
+    password: constr(max_length=128)
+
--- users/views.py
+++ users/views.py
@@ -1,20 +1,26 @@
+import uuid
+from typing import List
+
 from fastapi import Depends, HTTPException
 from starlette import status as http_status
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from starlette.responses import JSONResponse
 
 from core.db import get_session
+from core.extensions import hashManager
 
 from users import users_router
 from users.model import User as UserModel
-from users.scheme import CreateUserScheme
+from users.scheme import CreateUserScheme, DumpUserScheme, UpdateUserScheme
 
 
-@users_router.post("/")
+@users_router.post("/", response_model=DumpUserScheme)
 async def create_user(
     user_data: CreateUserScheme, db_session: so.Session = Depends(get_session)
 ):
+    """creating new user in database."""
     query = sa.select(UserModel).filter(
         sa.or_(
             UserModel.username == user_data.username,
@@ -55,19 +61,83 @@ async def create_user(
             detail=f"there was an error in the saving the user in db. check logs for more info. + {e.args}",
         )
     db_session.refresh(new_user)
-    return {"msg": "user created successfully", "user_info": new_user}
+    return new_user
 
 
-@users_router.get("/{user_id}")
-async def get_user(user_id: int, db_session: so.Session = Depends(get_session)):
-    return {"msg": "OK"}
+@users_router.get("/id/{user_id}", response_model=DumpUserScheme)
+async def get_user_by_id(user_id: int, db_session: so.Session = Depends(get_session)):
+    """retrieve a user with  id """
+    query = sa.select(UserModel).filter_by(id=user_id)
+    result = db_session.execute(query).scalar_one_or_none()
+    if not result:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail="there is no user with given id.")
+    return result
+
+@users_router.get("/username/{username}", response_model=DumpUserScheme)
+async def get_user_by_username(username: str, db_session: so.Session = Depends(get_session)):
+    """retrieve a user with a username """
+    query = sa.select(UserModel).filter_by(username=username)
+    result = db_session.execute(query).scalar_one_or_none()
+    if not result:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail="there is no user with given username.")
+    return result
 
 
-@users_router.put("/{user_id}")
-async def update_user(user_id: int, db_session: so.Session = Depends(get_session)):
-    return {"msg": "OK"}
+@users_router.get("/public_key/{public_key}", response_model=DumpUserScheme)
+async def get_user_by_public_key(public_key: str, db_session: so.Session = Depends(get_session)):
+    """retrieve a user with a public-key """
+    query = sa.select(UserModel).filter_by(public_key=public_key)
+    result = db_session.execute(query).scalar_one_or_none()
+    if not result:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail="there is no user with given public_key.")
+    return result
 
+@users_router.get("/", response_model=List[DumpUserScheme])
+async def get_all_users(db_session: so.Session = Depends(get_session)):
+    """returns all users in pagination"""
+    # TODO: add pagination
+    query = sa.select(UserModel)
+    result = db_session.execute(query).scalars().all()
+    if not result:
+        raise HTTPException(status_code=http_status.HTTP_404_NOT_FOUND, detail="there is no user in db.")
+    return result
 
-@users_router.delete("/{user_id}")
-async def delete_user(user_id: int, db_session: so.Session = Depends(get_session)):
-    return {"msg": "OK"}
+@users_router.put("/{user_id}", status_code=http_status.HTTP_204_NO_CONTENT)
+async def update_user(user_id: int, user_data: UpdateUserScheme, db_session: so.Session = Depends(get_session)):
+    """Update a specific user"""
+    user_data = user_data.model_dump()
+    user_data["password"] = hashManager.hash(user_data["password"])
+    query = sa.update(UserModel).where(UserModel.id==user_id).values(
+        **user_data
+    )
+    try:
+        result = db_session.execute(query)
+        db_session.commit()
+        if result.rowcount > 0:
+            return {}
+        else:
+            raise HTTPException(status_code=http_status.HTTP_400_BAD_REQUEST, detail="User not found or no changes made")
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
+                             detail="An error occurred")
+@users_router.delete("/id/{user_id}", status_code=http_status.HTTP_204_NO_CONTENT)
+async def delete_user_by_id(user_id: int, db_session: so.Session = Depends(get_session)):
+    """delete a user with given user id"""
+    query = sa.delete(UserModel).filter_by(id=user_id)
+    try:
+        result = db_session.execute(query)
+        db_session.commit()
+        if result.rowcount > 0:
+            return {}
+        else:
+            raise HTTPException(status_code=http_status.HTTP_400_BAD_REQUEST, detail="User not found or no changes made")
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        raise HTTPException(
+            status_code=http_status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred"
+        )
+
+    return {}
