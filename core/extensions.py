40f589d1b060c7095cb1c3acbb9d30438a37c28d
ali sharifi <alisharifyofficial@gmail.com>
2025-04-12 09:01:15 +0330

    add rabbitManager to extensions.py

--- core/extensions.py
+++ core/extensions.py
@@ -1,3 +1,25 @@
+"""
+* users management
+* author: github.com/alisharify7
+* email: alisharifyofficial@gmail.com
+* license: see LICENSE for more details.
+* Copyright (c) 2025 - ali sharifi
+* https://github.com/alisharify7/user-service-management
+"""
+
+from core.config import get_config
+
 from passlib.context import CryptContext
+from common_libs.rabbitmq import RabbitMQManger
+
+
+Setting = get_config()
 
 hashManager = CryptContext(schemes=["bcrypt"], deprecated="auto")
+rabbitManager = RabbitMQManger(
+    username=Setting.RABBITMQ_USERNAME,
+    password=Setting.RABBITMQ_PASSWORD,
+    host=Setting.RABBITMQ_HOST,
+    port=Setting.RABBITMQ_PORT,
+    virtual_host=Setting.RABBITMQ_VHOST,
+)
